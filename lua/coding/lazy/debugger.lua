b3210822567986cdd9796e75558f40505309e4ac
Markus Siegert <spongemarkus@gmail.com>
2024-03-21 11:53:15 +0100

    update and installing of nvim nio

--- lua/coding/lazy/debugger.lua
+++ lua/coding/lazy/debugger.lua
@@ -3,6 +3,7 @@ return {
         "rcarriga/nvim-dap-ui",
         dependencies = {
             "mfussenegger/nvim-dap",
+            "nvim-neotest/nvim-nio",
         },
         config = function(_, opts)
             local dapui = require("dapui")
