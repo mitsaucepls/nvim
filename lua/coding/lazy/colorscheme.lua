f6b8ed9cc845899b1afed097824303486e59cbde
Markus Siegert <markus.siegert@telekom.de>
2024-07-12 16:55:44 +0200

    new colorscheme

--- lua/coding/lazy/colorscheme.lua
+++ lua/coding/lazy/colorscheme.lua
@@ -2,30 +2,44 @@
 vim.opt.list = true
 vim.opt.listchars = "tab:→\\x20,space:·"
 
+local function linenumbercolor()
+    vim.api.nvim_set_hl(0, 'LineNrAbove', { bold=true })
+    vim.api.nvim_set_hl(0, 'LineNr', { bold=true })
+    vim.api.nvim_set_hl(0, 'LineNrBelow', { bold=true })
+    vim.api.nvim_set_hl(0, 'Comment', { bold=true })
+    -- Set the initial highlight for CursorLine
+    vim.api.nvim_set_hl(0, "CursorLine", {default=true, blend=50})
+    vim.api.nvim_set_hl(0, "NormalFloat", {bg="NONE"})
+    vim.api.nvim_set_hl(0, "FloatBorder", {bg="NONE"})
+end
+
 return {
+    -- {
+    --     'folke/tokyonight.nvim',
+    --     lazy = false,
+    --     priority = 1000,
+    --     config = function()
+    --         require("tokyonight").setup({
+    --             style = "storm",
+    --             transparent = true,
+    --             terminal_colors = true,
+    --             styles = {
+    --                 comments = { italic = false },
+    --                 keywords = { italic = false },
+    --                 sidebars = "transparent",
+    --                 floats = "transparent",
+    --             },
+    --         })
+    --         vim.cmd.colorscheme("tokyonight")
+    --         linenumbercolor()
+    --     end
+    -- },
+
     {
-        'folke/tokyonight.nvim',
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require("tokyonight").setup({
-                style = "storm",
-                transparent = true,
-                terminal_colors = true,
-                styles = {
-                    comments = { italic = false },
-                    keywords = { italic = false },
-                    sidebars = "transparent",
-                    floats = "transparent",
-                },
-            })
-            vim.cmd.colorscheme("tokyonight")
-            vim.api.nvim_set_hl(0, 'LineNrAbove', { bold=true })
-            vim.api.nvim_set_hl(0, 'LineNr', { bold=true })
-            vim.api.nvim_set_hl(0, 'LineNrBelow', { bold=true })
-            vim.api.nvim_set_hl(0, 'Comment', { bold=true })
-            -- Set the initial highlight for CursorLine
-            vim.api.nvim_set_hl(0, "CursorLine", {default=true, blend=50})
+        "tjdevries/colorbuddy.nvim",
+        config = function ()
+            vim.cmd.colorscheme("gruvbuddy")
+            linenumbercolor()
         end
     },
 
