efba1ca46061a694a5653919b2ea60879639df00
Markus Siegert <markus.siegert@telekom.de>
2024-07-24 21:29:41 +0200

    indentation for markdown

--- lua/coding/indentation.lua
+++ lua/coding/indentation.lua
@@ -1,4 +1,4 @@
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "html,javascript,typescript,json,css,scss,vue,dart,yaml",
+    pattern = "html,javascript,typescript,json,css,scss,vue,dart,yaml,markdown",
     command = "setlocal shiftwidth=2 tabstop=2"
 })
