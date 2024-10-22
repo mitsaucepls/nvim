e7f7b0ff4099ac921e63bb23cda300b0746de877
Markus Siegert <spongebobmarkus@gmail.com>
2024-10-23 01:04:36 +0200

    idk

--- lua/coding/indentation.lua
+++ lua/coding/indentation.lua
@@ -1,4 +1,4 @@
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "html,javascript,typescript,json,css,scss,vue,dart,yaml,markdown,typescriptreact",
+    pattern = "html,javascript,typescript,json,css,scss,vue,dart,yaml,markdown,typescriptreact,nix",
     command = "setlocal shiftwidth=2 tabstop=2"
 })
