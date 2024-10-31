7e2108a4208c168f0d45610d1c309ef8ef3b447a
Markus Siegert <spongebobmarkus@gmail.com>
2024-10-31 20:47:05 +0100

    jq and yq parsing

--- lua/coding/remap.lua
+++ lua/coding/remap.lua
@@ -33,8 +33,10 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+-- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+-- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<leader>jq", "<cmd>!jq .<CR>")
+vim.keymap.set("n", "<leader>yq", "<cmd>!yq e -i . %<CR>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
