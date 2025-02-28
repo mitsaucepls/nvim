ff77b25f7d50a3b56ad29ba571fceaa3c6e0a393
Markus Siegert <spongebobmarkus@gmail.com>
2025-02-28 10:02:07 +0100

    chore: terraform indentation

--- lua/coding/indentation.lua
+++ lua/coding/indentation.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "r,html,javascript,typescript,json,css,scss,vue,dart,yaml,markdown,typescriptreact,nix",
+    pattern = "r,html,javascript,typescript,json,css,scss,vue,dart,yaml,markdown,typescriptreact,nix,terraform",
     command = "setlocal shiftwidth=2 tabstop=2"
 })
 vim.api.nvim_create_autocmd("FileType", {
