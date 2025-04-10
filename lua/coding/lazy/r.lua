bbad360f3fded5233359c4b41814d6d198749600
Markus Siegert <spongebobmarkus@gmail.com>
2025-04-10 09:30:45 +0200

    auto install libs in r

--- lua/coding/lazy/r.lua
+++ lua/coding/lazy/r.lua
@@ -1,10 +1,9 @@
 return {
     "R-nvim/R.nvim",
-    version = "~0.1.0",
     lazy = false,
     opts = {
         R_args = { "--quiet" },
-        external_term = "tmux split-window -v -l 40",
+        external_term = "tmux split-window -v -l $(($(tmux display -p '#{window_height}') / 2))",
         auto_quit = true,
     },
     keys = {
@@ -12,6 +11,24 @@ return {
             "<LocalLeader>gd",
             "<cmd>lua require('r.send').cmd('tryCatch(httpgd::hgd_browse(),error=function(e) {httpgd::hgd();httpgd::hgd_browse()})')<CR>",
             desc = "httpgd",
+        },
+        {
+            "<LocalLeader>ii",
+            function()
+                local filename = vim.fn.expand("%:p") -- absolute path of current file
+                local r_code = string.format([[
+                    lines <- readLines("%s")
+                    pkgs <- unique(unlist(regmatches(lines, gregexpr("(?<=library\\(|require\\()[\"']?[a-zA-Z0-9\\.]+[\"']?", lines, perl=TRUE))))
+                    pkgs <- gsub("[\"']", "", pkgs)
+                    for (pkg in pkgs) {
+                        if (!requireNamespace(pkg, quietly = TRUE)) install.packages(pkg)
+                    }
+                    lapply(pkgs, library, character.only = TRUE)
+                ]], filename)
+
+                require("r.send").cmd(r_code)
+            end,
+            desc = "Install R libraries",
         },
     }
 }
