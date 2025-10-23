c47f2c8be17e9bd09cf5523fb9847630588f9994
Markus Siegert <markus.siegert@linux.com>
2025-10-23 20:10:54 +0200

    fix: rm icons

--- lua/coding/lazy/oil.lua
+++ lua/coding/lazy/oil.lua
@@ -9,7 +9,7 @@ return {
     },
   },
   -- Optional dependencies
-  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+  -- dependencies = { { "nvim-mini/mini.icons", opts = {} } },
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
 }
