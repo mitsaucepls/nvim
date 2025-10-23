d1d60ad3364aed7b39d865363b21251802514319
Markus Siegert <markus.siegert@linux.com>
2025-10-23 17:26:31 +0200

    chore: chore

--- lua/coding/lazy/oil.lua
+++ lua/coding/lazy/oil.lua
@@ -2,13 +2,14 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    view_options = {
+      -- Show files and directories that start with "."
+      show_hidden = true,
+    },
+  },
   -- Optional dependencies
   dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-  config = function ()
-    require("oil").setup()
-  end,
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
 }
