8cc825356ea6c00ed64bc5392fcd0d7e55fb4242
Markus Siegert <markus.siegert@linux.com>
2026-02-14 13:31:45 +0100

    ci: filename command

--- lua/coding/remap.lua
+++ lua/coding/remap.lua
@@ -10,12 +10,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end)
-
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
+vim.keymap.set("n", "<leader>fn", function()
+  print(vim.fn.expand('%:p:.'))
 end)
 
 -- greatest remap ever
