946f6369dda5d385c9a55d0a157b3e4dad86f142
Markus Siegert <markus.siegert@linux.com>
2026-03-19 10:21:16 +0100

    rm: nvim-r

--- lua/coding/lazy/r.lua
+++ lua/coding/lazy/r.lua
@@ -1,34 +1,34 @@
 return {
-    "R-nvim/R.nvim",
-    lazy = false,
-    opts = {
-        R_args = { "--quiet" },
-        external_term = "tmux split-window -v -l $(($(tmux display -p '#{window_height}') / 2))",
-        auto_quit = true,
-    },
-    keys = {
-        {
-            "<LocalLeader>gd",
-            "<cmd>lua require('r.send').cmd('tryCatch(httpgd::hgd_browse(),error=function(e) {httpgd::hgd();httpgd::hgd_browse()})')<CR>",
-            desc = "httpgd",
-        },
-        {
-            "<LocalLeader>ii",
-            function()
-                local filename = vim.fn.expand("%:p") -- absolute path of current file
-                local r_code = string.format([[
-                    lines <- readLines("%s")
-                    pkgs <- unique(unlist(regmatches(lines, gregexpr("(?<=library\\(|require\\()[\"']?[a-zA-Z0-9\\.]+[\"']?", lines, perl=TRUE))))
-                    pkgs <- gsub("[\"']", "", pkgs)
-                    for (pkg in pkgs) {
-                        if (!requireNamespace(pkg, quietly = TRUE)) install.packages(pkg)
-                    }
-                    lapply(pkgs, library, character.only = TRUE)
-                ]], filename)
+    -- "R-nvim/R.nvim",
+    -- lazy = false,
+    -- opts = {
+    --     R_args = { "--quiet" },
+    --     external_term = "tmux split-window -v -l $(($(tmux display -p '#{window_height}') / 2))",
+    --     auto_quit = true,
+    -- },
+    -- keys = {
+    --     {
+    --         "<LocalLeader>gd",
+    --         "<cmd>lua require('r.send').cmd('tryCatch(httpgd::hgd_browse(),error=function(e) {httpgd::hgd();httpgd::hgd_browse()})')<CR>",
+    --         desc = "httpgd",
+    --     },
+    --     {
+    --         "<LocalLeader>ii",
+    --         function()
+    --             local filename = vim.fn.expand("%:p") -- absolute path of current file
+    --             local r_code = string.format([[
+    --                 lines <- readLines("%s")
+    --                 pkgs <- unique(unlist(regmatches(lines, gregexpr("(?<=library\\(|require\\()[\"']?[a-zA-Z0-9\\.]+[\"']?", lines, perl=TRUE))))
+    --                 pkgs <- gsub("[\"']", "", pkgs)
+    --                 for (pkg in pkgs) {
+    --                     if (!requireNamespace(pkg, quietly = TRUE)) install.packages(pkg)
+    --                 }
+    --                 lapply(pkgs, library, character.only = TRUE)
+    --             ]], filename)
 
-                require("r.send").cmd(r_code)
-            end,
-            desc = "Install R libraries",
-        },
-    }
+    --             require("r.send").cmd(r_code)
+    --         end,
+    --         desc = "Install R libraries",
+    --     },
+    -- }
 }
